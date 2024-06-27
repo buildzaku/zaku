6dd9b12f619d783318175aeef8b5c73f04cba8ee
Mayank Verma <errmayank@gmail.com>
2024-06-27 19:42:47 +0530

    chore: setup Tailwind, shadcn/ui and themes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<div class="w-dvw h-dvh flex justify-center items-center">
-    <h1>Zaku</h1>
+<script>
+    import { Button } from "$lib/components/ui/button";
+    import { toggleMode } from "mode-watcher";
+</script>
+
+<div class="w-dvw h-dvh flex flex-col gap-4 justify-center items-center">
+    <h1 class="text-4xl font-semibold">Zaku</h1>
+    <Button on:click={toggleMode}>Hello!</Button>
 </div>
