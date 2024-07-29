1dfd6323ccb10e154c93c1d7c23b8f35edcf3063
Mayank Verma <errmayank@gmail.com>
2024-07-29 21:33:06 +0530

    chore(metadata): sync snapcraft.yaml with latest metadata

--- scripts/sync-metadata/src/main.rs
+++ scripts/sync-metadata/src/main.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use serde_yaml::{from_str, to_string, Value};
 use std::fs;
 use std::path::PathBuf;
 use toml_edit::{value, Array, DocumentMut};
@@ -17,6 +18,7 @@ struct PackageJson {
 fn main() -> std::io::Result<()> {
     let package_json_path = PathBuf::from("./../../package.json");
     let cargo_toml_path = PathBuf::from("./../../src-tauri/Cargo.toml");
+    let snapcraft_yaml_path = PathBuf::from("./../../snapcraft.yaml");
 
     let package_json_content = fs::read_to_string(package_json_path).unwrap();
     let package_json: PackageJson = serde_json::from_str(&package_json_content).unwrap();
@@ -24,20 +26,30 @@ fn main() -> std::io::Result<()> {
     let cargo_toml_content = fs::read_to_string(&cargo_toml_path).unwrap();
     let mut cargo_toml: DocumentMut = cargo_toml_content.parse::<DocumentMut>().unwrap();
 
-    cargo_toml["package"]["name"] = value(package_json.name);
-    cargo_toml["package"]["version"] = value(package_json.version);
-    cargo_toml["package"]["description"] = value(package_json.description);
-    cargo_toml["package"]["license"] = value(package_json.license);
+    cargo_toml["package"]["name"] = value(package_json.name.clone());
+    cargo_toml["package"]["version"] = value(package_json.version.clone());
+    cargo_toml["package"]["description"] = value(package_json.description.clone());
+    cargo_toml["package"]["license"] = value(package_json.license.clone());
 
     let mut authors_array = Array::new();
-    authors_array.push(package_json.author);
+    authors_array.push(package_json.author.clone());
     cargo_toml["package"]["authors"] = value(authors_array);
 
-    cargo_toml["package"]["repository"] = value(package_json.repository);
-    cargo_toml["package"]["homepage"] = value(package_json.homepage);
+    cargo_toml["package"]["repository"] = value(package_json.repository.clone());
+    cargo_toml["package"]["homepage"] = value(package_json.homepage.clone());
 
     fs::write(&cargo_toml_path, cargo_toml.to_string()).unwrap();
 
-    println!("Synced Cargo.toml metadata with package.json");
+    let snapcraft_yaml_content = fs::read_to_string(&snapcraft_yaml_path).unwrap();
+    let mut snapcraft_yaml: Value = from_str(&snapcraft_yaml_content).unwrap();
+
+    snapcraft_yaml["name"] = Value::String(package_json.name);
+    snapcraft_yaml["version"] = Value::String(package_json.version);
+    snapcraft_yaml["summary"] = Value::String(package_json.description);
+
+    let snapcraft_yaml_string = to_string(&snapcraft_yaml).unwrap();
+    fs::write(&snapcraft_yaml_path, snapcraft_yaml_string).unwrap();
+
+    println!("Synced Cargo.toml and snapcraft.yaml metadata with package.json");
     Ok(())
 }
