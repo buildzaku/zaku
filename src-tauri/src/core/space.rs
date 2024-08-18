7243b2b657c886933affdc1f37285a1ca01b9dbb
Mayank Verma <errmayank@gmail.com>
2024-08-18 19:45:07 +0530

    clean1

--- src-tauri/src/core/space.rs
+++ src-tauri/src/core/space.rs
@@ -190,45 +190,36 @@ pub fn delete_active_space(
     })
     .unwrap();
 
-    *state.lock().unwrap() = AppState {
-        active_space: None,
-    };
+    *state.lock().unwrap() = AppState { active_space: None };
 }
 
 pub fn parse_space(path: &Path) -> Result<Space, Error> {
-    // Initialize a vector to store collections
-    let mut collections: Vec<Collection> = Vec::new();
-    // Initialize a vector to store requests found at the root level
-    let mut requests: Vec<Request> = Vec::new();
-
-    // Create the full path to the config.toml file in the space
-    let config_path = path.join(".zaku/config.toml");
-
-    // Reading the config file with error context
-    let space_config_content = fs::read_to_string(&config_path).map_err(|e| {
-        Error::new(
-            ErrorKind::NotFound,
-            format!("Failed to load {}: {}", config_path.display(), e),
-        )
-    })?;
-
-    // Parsing space config with error context
-    let space_config: SpaceConfig =
-        toml::from_str(&space_config_content).map_err(|err| {
+    let root_path = path;
+    let config_path = root_path.join(".zaku/config.toml");
+    let space_config: SpaceConfig = fs::read_to_string(&config_path)
+        .map_err(|e| {
             Error::new(
-                ErrorKind::InvalidData,
-                format!("Failed to parse {}: {}", config_path.display(), err),
+                ErrorKind::NotFound,
+                format!("Failed to load {}: {}", config_path.display(), e),
             )
+        })
+        .and_then(|content| {
+            toml::from_str(&content).map_err(|err| {
+                Error::new(
+                    ErrorKind::InvalidData,
+                    format!("Failed to parse {}: {}", config_path.display(), err),
+                )
+            })
         })?;
 
-    // Stack to keep track of directories to process, starting with the root space directory
-    let mut directories_to_process: VecDeque<PathBuf> = VecDeque::new();
-    directories_to_process.push_back(path.to_path_buf());
+    let mut collections: Vec<Collection> = Vec::new();
+    let mut requests: Vec<Request> = Vec::new();
 
-    // Loop until all directories have been processed
-    while let Some(current_directory) = directories_to_process.pop_front() {
-        // Iterate over the entries in the current directory
-        for directory_entry in fs::read_dir(&current_directory).map_err(|err| {
+    let mut directories: VecDeque<PathBuf> = VecDeque::new();
+    directories.push_back(root_path.to_path_buf());
+
+    while let Some(current_directory) = directories.pop_front() {
+        let sub_directories = fs::read_dir(&current_directory).map_err(|err| {
             Error::new(
                 ErrorKind::Other,
                 format!(
@@ -237,65 +228,62 @@ pub fn parse_space(path: &Path) -> Result<Space, Error> {
                     err
                 ),
             )
-        })? {
-            let directory_entry = directory_entry.map_err(|e| {
-                Error::new(
-                    ErrorKind::Other,
-                    format!(
-                        "Failed to access entry in {}: {}",
-                        current_directory.display(),
-                        e
-                    ),
-                )
-            })?;
-            let entry_path = directory_entry.path(); // Get the full path of the entry
+        })?;
+
+        for sub_directory in sub_directories {
+            let entry_path = sub_directory
+                .map_err(|err| {
+                    Error::new(
+                        ErrorKind::Other,
+                        format!(
+                            "Failed to access sub directory in {}: {}",
+                            current_directory.display(),
+                            err
+                        ),
+                    )
+                })?
+                .path();
             let entry_name = entry_path
                 .file_name()
                 .unwrap()
                 .to_string_lossy()
-                .into_owned(); // Get the entry's name as a string
+                .into_owned();
 
             if entry_path.is_file() {
-                // If the entry is a file and it ends with ".toml"
                 if entry_name.ends_with(".toml") {
-                    if current_directory == path {
-                        // If the file is at the root level, add it to the requests vector
+                    if current_directory == root_path {
                         requests.push(Request { name: entry_name });
                     } else {
-                        // If the file is inside a collection folder, add it to the appropriate collection
-                        let parent_folder_name = current_directory
+                        let parent_directory = current_directory
                             .file_name()
                             .unwrap()
                             .to_string_lossy()
                             .into_owned();
                         if let Some(collection) = collections
                             .iter_mut()
-                            .find(|c| c.name == parent_folder_name)
+                            .find(|collection| collection.name == parent_directory)
                         {
                             collection.requests.push(Request { name: entry_name });
                         }
                     }
                 }
             } else if entry_path.is_dir() {
-                // If the entry is a directory
-                if entry_name == ".zaku" && current_directory != path {
-                    // Skip nested .zaku directories (only the root .zaku is allowed)
+                if entry_name == ".zaku" && current_directory != root_path {
                     continue;
-                } else if current_directory == path && entry_name != ".zaku" {
-                    // If the directory is not .zaku and is at the root level, treat it as a collection
+                } else if current_directory == root_path && entry_name != ".zaku" {
                     collections.push(Collection {
-                        name: entry_name.clone(), // Name of the collection (folder name)
-                        requests: Vec::new(),     // Initialize with an empty vector of requests
+                        name: entry_name.clone(),
+                        requests: Vec::new(),
                     });
-                    // Add this directory to the stack to process its contents
-                    directories_to_process.push_back(entry_path);
+
+                    directories.push_back(entry_path);
                 }
             }
         }
     }
 
     return Ok(Space {
-        path: path.to_string_lossy().into_owned(),
+        path: root_path.to_string_lossy().into_owned(),
         config: space_config,
         collections,
         requests,
