87e04dfdbc3c13d64e615a79b0990280cfd32238
Mayank Verma <errmayank@gmail.com>
2024-08-19 19:12:25 +0530

    rename

--- src-tauri/src/core/commands/dialog.rs
+++ src-tauri/src/core/commands/dialog.rs
@@ -3,19 +3,19 @@ use tauri::AppHandle;
 use tauri_plugin_dialog::DialogExt;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct OpenDirectoryDialog {
+pub struct OpenDirectoryDialogOptions {
     pub title: Option<String>,
 }
 
 #[tauri::command]
 pub async fn open_directory_dialog<R: tauri::Runtime>(
-    options: Option<OpenDirectoryDialog>,
+    options: Option<OpenDirectoryDialogOptions>,
     app_handle: AppHandle<R>,
 ) -> Result<Option<String>, String> {
     let mut dialog_builder = app_handle.dialog().file();
 
     match options {
-        Some(OpenDirectoryDialog {
+        Some(OpenDirectoryDialogOptions {
             title: Some(ref title),
         }) => {
             dialog_builder = dialog_builder.set_title(title);
