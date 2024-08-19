8c12516f3f773a9336c7161f947b93980293066b
mayank <errmayank@gmail.com>
2024-08-20 00:35:06 +0530

    feat(notifications): implement dispatch, check & request commands for notification (#12)

---
+++ src-tauri/src/core/commands/notification.rs
@@ -0,0 +1,53 @@
+use serde::{Deserialize, Serialize};
+use tauri::AppHandle;
+use tauri_plugin_notification::{NotificationExt, PermissionState};
+
+use crate::types::ZakuError;
+
+#[tauri::command]
+pub fn is_notification_permission_granted(app_handle: AppHandle) -> Result<bool, ZakuError> {
+    let permission_state = app_handle
+        .notification()
+        .permission_state()
+        .map_err(|err| ZakuError {
+            error: format!("Failed to get permission state: {}", err),
+        })?;
+
+    return Ok(permission_state == PermissionState::Granted);
+}
+
+#[tauri::command]
+pub fn request_notification_permission(app_handle: AppHandle) -> Result<bool, ZakuError> {
+    let permission_state = app_handle
+        .notification()
+        .request_permission()
+        .map_err(|err| ZakuError {
+            error: format!("Failed to request permission: {}", err),
+        })?;
+
+    return Ok(permission_state == PermissionState::Granted);
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DispatchNotificationOptions {
+    pub title: String,
+    pub body: String,
+}
+
+#[tauri::command]
+pub fn dispatch_notification(
+    options: DispatchNotificationOptions,
+    app_handle: AppHandle,
+) -> Result<(), ZakuError> {
+    app_handle
+        .notification()
+        .builder()
+        .title(&options.title)
+        .body(&options.body)
+        .show()
+        .map_err(|err| ZakuError {
+            error: format!("Failed to dispatch notification: {}", err),
+        })?;
+
+    return Ok(());
+}
--- src/lib/commands/index.ts
+++ src/lib/commands/index.ts
@@ -1,11 +1,34 @@
 import { invoke } from "@tauri-apps/api/core";
 
-export type OpenDirectoryDialog = {
+export type OpenDirectoryDialogOptions = {
     title?: string;
 };
 
-export async function openDirectoryDialog(options?: OpenDirectoryDialog) {
+export async function openDirectoryDialog(options?: OpenDirectoryDialogOptions) {
     const path: string | null = await invoke("open_directory_dialog", { options });
 
     return path;
+}
+
+export type DispatchNotificationOptions = {
+    title: string;
+    body: string;
+};
+
+export async function dispatchNotification(options: DispatchNotificationOptions) {
+    const isNotificationPermissionGranted: boolean = await invoke(
+        "is_notification_permission_granted",
+    );
+
+    if (isNotificationPermissionGranted) {
+        await invoke("dispatch_notification", { options });
+    } else {
+        const isNotificationPermissionGrantedAfterRequest: boolean = await invoke(
+            "request_notification_permission",
+        );
+
+        if (isNotificationPermissionGrantedAfterRequest) {
+            await invoke("dispatch_notification", { options });
+        }
+    }
 }
