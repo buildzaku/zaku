0b6a48fe7abd9bf4ef0cc77a7b06eaa3f56ad178
mayank <errmayank@gmail.com>
2024-09-19 00:50:03 +0530

    fix(space): display toast if space is invalid on switch (#17)
    - replace non-reactive select with dropdown component for switcher
    - safe run wrapper functions for sync and async
    - reduce animations

--- src/lib/components/sidebar/sidebar.svelte
+++ src/lib/components/sidebar/sidebar.svelte
@@ -1,52 +1,44 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
     import { zakuState } from "$lib/store";
     import { Button } from "$lib/components/primitives/button";
-    import {
-        DropdownMenu,
-        DropdownMenuContent,
-        DropdownMenuItem,
-        DropdownMenuTrigger,
-    } from "$lib/components/primitives/dropdown-menu";
+    import type { PaneAPI } from "paneforge";
 
-    import { CookieIcon, SettingsIcon, Trash2Icon, PlusIcon } from "lucide-svelte";
-    import { SpaceCreateDialog, SpaceSwitcher } from "$lib/components/space";
+    import { CookieIcon, SettingsIcon, PanelLeftIcon } from "lucide-svelte";
+    import { SpaceSwitcher } from "$lib/components/space";
     import { cn } from "$lib/utils/style";
-    import { dispatchNotification, getSpaceReference, openDirectoryDialog } from "$lib/commands";
 
-    export let isCollapsed = false;
-
-    let isCreateSpaceDialogOpen = false;
-
-    async function handleOpenExistingSpace() {
-        try {
-            const selectedPath = await openDirectoryDialog({ title: "Open an existing Space" });
-
-            if (selectedPath !== null) {
-                const spaceReference = await getSpaceReference(selectedPath);
-                await zakuState.set(spaceReference);
-                await goto("/space");
-            }
-        } catch (err) {
-            console.error(err);
-            await dispatchNotification({
-                title: "Doesn't look like a valid space.",
-                body: "Unable to parse the directory, make sure it is a valid space and try again.",
-            });
-        }
-    }
-
-    async function handleDelete() {
-        if ($zakuState.active_space) {
-            await zakuState.delete($zakuState.active_space.path);
-        }
-    }
+    export let pane: PaneAPI;
+    export let isCollapsed: boolean;
 </script>
 
 {#if $zakuState.active_space}
     <div class="flex size-full flex-col justify-between">
-        <div class="flex w-full items-center justify-center border-b p-1.5">
-            <SpaceSwitcher {isCollapsed} />
+        <div class="mt-1.5 flex w-full items-center justify-center border-b p-1.5">
+            <div
+                class={cn(
+                    "flex w-full items-center justify-between gap-1.5",
+                    isCollapsed ? "flex-col" : "flex-row",
+                )}
+            >
+                <div class="flex-grow overflow-hidden text-ellipsis whitespace-nowrap">
+                    <SpaceSwitcher isSidebarCollapsed={isCollapsed} />
+                </div>
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    on:click={() => {
+                        if (isCollapsed) {
+                            pane.expand();
+                            pane.resize(24);
+                        } else {
+                            pane.collapse();
+                        }
+                    }}
+                    class="flex-shrink-0"
+                >
+                    <PanelLeftIcon size={14} class="min-h-[14px] min-w-[14px]" />
+                </Button>
+            </div>
         </div>
         <div class="flex-grow overflow-y-auto p-1.5">
             {#if !isCollapsed}
@@ -61,47 +53,13 @@
                 isCollapsed && "flex-col-reverse",
             )}
         >
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild let:builder>
-                    <Button builders={[builder]} size="icon" variant="ghost-hover">
-                        <SettingsIcon strokeWidth={1.25} size={16} />
-                        <span class="sr-only">Settings</span>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuItem
-                        on:click={() => {
-                            isCreateSpaceDialogOpen = true;
-                        }}
-                    >
-                        <div class="flex items-center gap-1.5 text-small">
-                            <PlusIcon strokeWidth={2.25} size={13} />
-                            <span>Create new Space</span>
-                        </div>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem on:click={handleOpenExistingSpace}>
-                        <div class="flex items-center gap-1.5 text-small">
-                            <PlusIcon strokeWidth={2.25} size={13} />
-                            <span>Open existing Space</span>
-                        </div>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem on:click={handleDelete}>
-                        <div class="flex items-center gap-1.5 text-small text-destructive">
-                            <Trash2Icon strokeWidth={2.25} size={13} />
-                            <span>Delete space</span>
-                        </div>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-            <Button size="icon" variant="ghost-hover">
+            <Button size="icon" variant="ghost">
+                <SettingsIcon strokeWidth={1.25} size={16} />
+                <span class="sr-only">Settings</span>
+            </Button>
+            <Button size="icon" variant="ghost">
                 <CookieIcon size={14} />
             </Button>
         </div>
     </div>
-    <SpaceCreateDialog
-        bind:isOpen={isCreateSpaceDialogOpen}
-        onCreate={async () => {
-            isCreateSpaceDialogOpen = false;
-        }}
-    />
 {/if}
