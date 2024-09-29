c68976533bfee1c04b567d04c46292651ef0155d
mayank <errmayank@gmail.com>
2024-09-29 23:45:19 +0530

    ui: add request and response pane handlers
    - improve dark theme colors
    - fix colors for request methods

--- src/lib/components/response-pane/response-pane.svelte
+++ src/lib/components/response-pane/response-pane.svelte
@@ -1,81 +1,154 @@
 <script lang="ts">
     import { Rocket } from "svelte-radix";
+    import type { PaneAPI } from "paneforge";
+    import { ChevronDownIcon, ChevronUpIcon } from "lucide-svelte";
 
     import { Tabs, TabsList, TabsTrigger, TabsContent } from "$lib/components/primitives/tabs";
     import type { RequestStatus } from "$lib/utils/api";
     import { Alert } from "$lib/components/primitives/alert";
     import { CodeBlock } from "$lib/components/code-block";
+    import { Button } from "$lib/components/primitives/button";
 
+    export let pane: PaneAPI;
+    export let isCollapsed: boolean;
     export let status: RequestStatus;
     export let raw: string;
     export let preview: string;
     export let error: string;
 </script>
 
-<div class="size-full bg-muted/25">
+<div class="size-full bg-card">
     {#if status === "idle"}
-        <div class="flex size-full items-center justify-center gap-2">
-            <Rocket size={20} />
-            <span>
-                Hit <b class="font-semibold">Send</b> to make a request
-            </span>
-        </div>
+        {#if isCollapsed}
+            <div class="flex h-8 w-full items-center justify-between border-b bg-accent/25">
+                <div class="flex size-full items-center justify-end">
+                    <Button
+                        variant="ghost"
+                        on:click={() => {
+                            pane.expand();
+                            pane.resize(60);
+                        }}
+                    >
+                        <span class="pr-1.5 text-xs font-medium">Response</span>
+                        <ChevronUpIcon size={14} />
+                    </Button>
+                </div>
+            </div>
+        {:else}
+            <div class="flex h-8 w-full items-center justify-between border-b bg-accent/25">
+                <div class="flex size-full items-center justify-end">
+                    <Button
+                        variant="ghost"
+                        on:click={() => {
+                            pane.collapse();
+                        }}
+                    >
+                        <span class="pr-1.5 text-xs font-medium">Response</span>
+                        <ChevronDownIcon size={14} />
+                    </Button>
+                </div>
+            </div>
+            <div class="flex size-full items-center justify-center gap-2 pb-8">
+                <Rocket size={20} />
+                <span>
+                    Hit <b class="font-semibold">Send</b> to make a request
+                </span>
+            </div>
+        {/if}
     {:else}
         <Tabs value="body" class="size-full">
-            <div class="w-full border-b bg-muted/30 px-3 py-1">
-                <TabsList
-                    class="grid auto-cols-min grid-flow-col justify-start gap-2 p-0 [&>*]:text-xs"
-                >
-                    <TabsTrigger value="body">Body</TabsTrigger>
-                    <TabsTrigger value="cookies">Cookies</TabsTrigger>
-                    <TabsTrigger value="headers">Headers</TabsTrigger>
-                </TabsList>
-            </div>
-            <div class="flex h-[calc(100%-2.25rem)] w-full items-center justify-center">
-                <TabsContent value="body" class="m-0 size-full">
-                    {#if status === "success"}
-                        <Tabs value="pretty" class="size-full">
-                            <div class="flex items-center justify-end border-b px-3">
-                                <TabsList class="my-1 auto-cols-min grid-flow-col gap-2 p-0">
-                                    <TabsTrigger value="pretty">Pretty</TabsTrigger>
-                                    <TabsTrigger value="raw">Raw</TabsTrigger>
-                                    <TabsTrigger value="preview">Preview</TabsTrigger>
-                                </TabsList>
-                            </div>
-                            <div class="h-[calc(100%-2.25rem)] w-full overflow-scroll [&>*]:m-0">
-                                <TabsContent value="pretty">
-                                    <CodeBlock lang="json" bind:value={raw} class="w-full" />
-                                </TabsContent>
-                                <TabsContent value="raw">
-                                    <CodeBlock lang="json" bind:value={raw} class="size-fit" />
-                                </TabsContent>
-                                <TabsContent value="preview" class="size-full">
-                                    <iframe
-                                        title=""
-                                        src="about:blank"
-                                        srcdoc={preview}
-                                        class="size-full"
-                                        loading="lazy"
-                                        sandbox=""
-                                    />
-                                </TabsContent>
-                            </div>
-                        </Tabs>
-                    {:else if status === "error"}
-                        <div class="flex size-full items-center justify-center gap-2">
-                            <Alert
-                                variant="destructive"
-                                class="w-fit max-w-[50%] py-1 [&>*]:select-text"
+            <div
+                class="flex h-8 w-full items-center justify-between border-y border-t-transparent bg-accent/25"
+            >
+                {#if isCollapsed}
+                    <div class="flex h-8 w-full items-center justify-between border-b">
+                        <div class="flex size-full items-center justify-end">
+                            <Button
+                                variant="ghost"
+                                on:click={() => {
+                                    pane.expand();
+                                    pane.resize(60);
+                                }}
                             >
-                                <span class="font-semibold">Error: </span>
-                                <span>{error}</span>
-                            </Alert>
+                                <span class="pr-1.5 text-xs font-medium">Response</span>
+                                <ChevronUpIcon size={14} />
+                            </Button>
                         </div>
-                    {/if}
-                </TabsContent>
-                <TabsContent value="cookies">WIP</TabsContent>
-                <TabsContent value="headers">WIP</TabsContent>
+                    </div>
+                {:else}
+                    <div class="px-1.5">
+                        <TabsList
+                            class="grid auto-cols-min grid-flow-col justify-start gap-2 p-0 [&>*]:text-xs"
+                        >
+                            <TabsTrigger value="body">Body</TabsTrigger>
+                            <TabsTrigger value="cookies">Cookies</TabsTrigger>
+                            <TabsTrigger value="headers">Headers</TabsTrigger>
+                        </TabsList>
+                    </div>
+                    <div class="flex h-8 w-full items-center justify-between border-b">
+                        <div class="flex size-full items-center justify-end">
+                            <Button
+                                variant="ghost"
+                                on:click={() => {
+                                    pane.collapse();
+                                }}
+                            >
+                                <span class="pr-1.5 text-xs font-medium">Response</span>
+                                <ChevronDownIcon size={14} />
+                            </Button>
+                        </div>
+                    </div>
+                {/if}
             </div>
+            {#if !isCollapsed}
+                <div class="flex h-[calc(100%-2.25rem)] w-full items-center justify-center">
+                    <TabsContent value="body" class="m-0 size-full">
+                        {#if status === "success"}
+                            <Tabs value="pretty" class="size-full">
+                                <div class="flex items-center justify-end border-b px-3">
+                                    <TabsList class="my-1 auto-cols-min grid-flow-col gap-2 p-0">
+                                        <TabsTrigger value="pretty">Pretty</TabsTrigger>
+                                        <TabsTrigger value="raw">Raw</TabsTrigger>
+                                        <TabsTrigger value="preview">Preview</TabsTrigger>
+                                    </TabsList>
+                                </div>
+                                <div
+                                    class="h-[calc(100%-2.25rem)] w-full overflow-scroll [&>*]:m-0"
+                                >
+                                    <TabsContent value="pretty">
+                                        <CodeBlock lang="json" bind:value={raw} class="w-full" />
+                                    </TabsContent>
+                                    <TabsContent value="raw">
+                                        <CodeBlock lang="json" bind:value={raw} class="size-fit" />
+                                    </TabsContent>
+                                    <TabsContent value="preview" class="size-full">
+                                        <iframe
+                                            title=""
+                                            src="about:blank"
+                                            srcdoc={preview}
+                                            class="size-full"
+                                            loading="lazy"
+                                            sandbox=""
+                                        />
+                                    </TabsContent>
+                                </div>
+                            </Tabs>
+                        {:else if status === "error"}
+                            <div class="flex size-full items-center justify-center gap-2">
+                                <Alert
+                                    variant="destructive"
+                                    class="w-fit max-w-[50%] py-1 [&>*]:select-text"
+                                >
+                                    <span class="font-semibold">Error: </span>
+                                    <span>{error}</span>
+                                </Alert>
+                            </div>
+                        {/if}
+                    </TabsContent>
+                    <TabsContent value="cookies">WIP</TabsContent>
+                    <TabsContent value="headers">WIP</TabsContent>
+                </div>
+            {/if}
         </Tabs>
     {/if}
 </div>
--- src/lib/utils/style.ts
+++ src/lib/utils/style.ts
@@ -8,20 +8,20 @@ export function cn(...inputs: ClassValue[]) {
 export const getMethodColorClass = (method: string) => {
     switch (method) {
         case "GET":
-            return "text-emerald-300";
+            return "text-emerald-300 data-[highlighted]:text-emerald-300";
         case "POST":
-            return "text-orange-300";
+            return "text-orange-300 data-[highlighted]:text-orange-300";
         case "DELETE":
-            return "text-red-300";
+            return "text-red-300 data-[highlighted]:text-red-300";
         case "PUT":
-            return "text-blue-300";
+            return "text-blue-300 data-[highlighted]:text-blue-300";
         case "PATCH":
-            return "text-indigo-300";
+            return "text-indigo-300 data-[highlighted]:text-indigo-300";
         case "HEAD":
-            return "text-amber-300";
+            return "text-amber-300 data-[highlighted]:text-amber-300";
         case "OPTIONS":
-            return "text-fuchsia-300";
+            return "text-fuchsia-300 data-[highlighted]:text-fuchsia-300";
         default:
-            return "text-gray-300";
+            return "text-gray-300 data-[highlighted]:text-gray-300";
     }
 };
