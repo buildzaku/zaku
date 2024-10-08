d98cf054dad75e6e6229d0e6e46ec3492cc62769
Mayank Verma <errmayank@gmail.com>
2024-10-08 20:32:04 +0530

    refactor: replace naming folder with dir

--- src-tauri/src/models/collection.rs
+++ src-tauri/src/models/collection.rs
@@ -6,7 +6,7 @@ use super::request::Request;
 #[derive(Clone, Debug, Serialize, Deserialize, TS)]
 #[ts(export, export_to = "../../src/lib/bindings.ts")]
 pub struct CollectionMeta {
-    pub folder_name: String,
+    pub dir_name: String,
     pub display_name: Option<String>,
     pub is_open: bool,
 }
