7ad87e3e9195df4630705e252b69a37f613f2baa
mayank <errmayank@gmail.com>
2024-10-14 21:12:15 +0530

    feat: add custom store for persistence

--- src-tauri/src/core/state.rs
+++ src-tauri/src/core/state.rs
@@ -1,20 +1,14 @@
 use std::path::PathBuf;
 use std::sync::Mutex;
 use tauri::{App, Manager};
-use tauri_plugin_store::StoreCollection;
 
 use super::{space, store};
 use crate::models::zaku::ZakuState;
 
 pub fn initialize(app: &mut App) {
-    let app_handle = app.handle();
-    let stores = app.state::<StoreCollection<tauri::Wry>>();
-
     let active_space_reference =
-        store::get_active_space_reference(app_handle.clone(), stores.clone()).or_else(|| {
-            space::find_first_valid_space_reference(app_handle.clone(), stores.clone())
-        });
-    let space_references = store::get_space_references(app_handle.clone(), stores.clone());
+        store::get_active_space_reference().or_else(|| space::find_first_valid_space_reference());
+    let space_references = store::get_space_references();
     let state = app.app_handle().state::<Mutex<ZakuState>>();
     let mut zaku_state = state.lock().unwrap();
 
@@ -25,29 +19,23 @@ pub fn initialize(app: &mut App) {
             Ok(active_space) => {
                 zaku_state.active_space = Some(active_space);
             }
-            Err(_) => {
-                match space::find_first_valid_space_reference(app_handle.clone(), stores.clone()) {
-                    Some(valid_space_reference) => {
-                        store::set_active_space_reference(
-                            valid_space_reference.clone(),
-                            app_handle.clone(),
-                            stores,
-                        );
+            Err(_) => match space::find_first_valid_space_reference() {
+                Some(valid_space_reference) => {
+                    store::set_active_space_reference(valid_space_reference.clone());
 
-                        let valid_space_path = PathBuf::from(valid_space_reference.path);
+                    let valid_space_path = PathBuf::from(valid_space_reference.path);
 
-                        match space::parse_space(&valid_space_path) {
-                            Ok(valid_space) => {
-                                zaku_state.active_space = Some(valid_space);
-                            }
-                            Err(err) => {
-                                eprintln!("Error parsing space: {}", err);
-                            }
+                    match space::parse_space(&valid_space_path) {
+                        Ok(valid_space) => {
+                            zaku_state.active_space = Some(valid_space);
+                        }
+                        Err(err) => {
+                            eprintln!("Error parsing space: {}", err);
                         }
                     }
-                    None => {}
                 }
-            }
+                None => {}
+            },
         };
     }
 
--- src-tauri/src/core/store.rs
+++ src-tauri/src/core/store.rs
@@ -1,164 +1,113 @@
-use tauri::{AppHandle, Manager, State, Wry};
-use tauri_plugin_store::StoreCollection;
+use once_cell::sync::Lazy;
+use postcard;
+use std::fs::{self, OpenOptions};
+use std::io::{Read, Write};
+use std::path::PathBuf;
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
-use crate::{constants::ZakuStoreKey, models::space::SpaceReference};
+use crate::models::space::SpaceReference;
+use crate::models::zaku::ZakuStore;
 
-pub fn get_active_space_reference(
-    app_handle: AppHandle,
-    stores: State<'_, StoreCollection<Wry>>,
-) -> Option<SpaceReference> {
-    let app_data_dir = app_handle.path().app_data_dir().unwrap();
+static STORE_ABSOLUTE_PATH: Lazy<PathBuf> = Lazy::new(|| {
+    dirs::data_dir()
+        .expect("Unable to get data directory")
+        .join("Zaku/store")
+        .with_extension("bin")
+});
 
-    return tauri_plugin_store::with_store(app_handle, stores, app_data_dir, |store| {
-        return Ok(store
-            .get(ZakuStoreKey::ActiveSpaceReference.to_string())
-            .and_then(|value| {
-                value.as_object().and_then(|object| {
-                    let path = object.get("path").unwrap().as_str().unwrap().to_string();
-                    let name = object.get("name").unwrap().as_str().unwrap().to_string();
+static ZAKU_STORE: Lazy<RwLock<ZakuStore>> = Lazy::new(|| {
+    if STORE_ABSOLUTE_PATH.exists() {
+        let mut file = fs::File::open(&*STORE_ABSOLUTE_PATH).expect("Failed to open Store file");
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)
+            .expect("Failed to read Store file contents");
+        let store_content: ZakuStore = postcard::from_bytes(&buffer).unwrap_or_default();
 
-                    return Some(SpaceReference { path, name });
-                })
-            }));
-    })
-    .unwrap_or(None);
+        RwLock::new(store_content)
+    } else {
+        RwLock::new(ZakuStore::default())
+    }
+});
+
+impl ZakuStore {
+    fn acquire_read_lock() -> RwLockReadGuard<'static, Self> {
+        ZAKU_STORE.read().expect("Failed to acquire read lock")
+    }
+
+    fn acquire_write_lock() -> RwLockWriteGuard<'static, Self> {
+        ZAKU_STORE.write().expect("Failed to acquire write lock")
+    }
+
+    fn persist(&self) {
+        if let Some(parent) = STORE_ABSOLUTE_PATH.parent() {
+            fs::create_dir_all(parent).expect("Failed to create parent directories");
+        }
+
+        let store_content = postcard::to_stdvec(self).expect("Failed to serialize store data");
+
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(&*STORE_ABSOLUTE_PATH)
+            .and_then(|mut file| file.write_all(&store_content))
+            .expect("Failed to write data");
+    }
 }
 
-pub fn set_active_space_reference(
-    space_reference: SpaceReference,
-    app_handle: AppHandle,
-    stores: State<'_, StoreCollection<Wry>>,
-) {
-    let app_data_dir = app_handle.path().app_data_dir().unwrap();
+pub fn get_active_space_reference() -> Option<SpaceReference> {
+    let zaku_store = ZakuStore::acquire_read_lock();
 
-    tauri_plugin_store::with_store(app_handle.clone(), stores.clone(), app_data_dir, |store| {
-        store
-            .insert(
-                ZakuStoreKey::ActiveSpaceReference.to_string(),
-                serde_json::json!({
-                    "path": space_reference.path,
-                    "name": space_reference.name,
-                }),
-            )
-            .unwrap();
-
-        store.save().unwrap();
-
-        return Ok(());
-    })
-    .unwrap();
+    return zaku_store.active_space_reference.clone();
 }
 
-pub fn delete_space_reference(
-    space_reference: SpaceReference,
-    app_handle: AppHandle,
-    stores: State<'_, StoreCollection<Wry>>,
-) {
-    let app_data_dir = app_handle.path().app_data_dir().unwrap();
+pub fn set_active_space_reference(space_reference: SpaceReference) {
+    let mut zaku_store = ZakuStore::acquire_write_lock();
+    zaku_store.active_space_reference = Some(space_reference);
 
-    if let Some(active_space_reference) =
-        get_active_space_reference(app_handle.clone(), stores.clone())
-    {
-        if active_space_reference.path == space_reference.path {
-            tauri_plugin_store::with_store(
-                app_handle.clone(),
-                stores.clone(),
-                app_data_dir,
-                |store| {
-                    store
-                        .delete(ZakuStoreKey::ActiveSpaceReference.to_string())
-                        .unwrap();
-                    store.save().unwrap();
+    ZakuStore::persist(&zaku_store);
+}
 
-                    return Ok(());
-                },
-            )
-            .unwrap()
+pub fn get_space_references() -> Vec<SpaceReference> {
+    let zaku_store = ZakuStore::acquire_read_lock();
+
+    return zaku_store.space_references.clone();
+}
+
+pub fn set_space_references(space_references: Vec<SpaceReference>) {
+    let mut zaku_store = ZakuStore::acquire_write_lock();
+    zaku_store.space_references = space_references;
+
+    ZakuStore::persist(&zaku_store);
+}
+
+pub fn insert_into_space_references_if_needed(space_reference: SpaceReference) {
+    let mut zaku_store = ZakuStore::acquire_write_lock();
+
+    let reference_exists = zaku_store
+        .space_references
+        .iter()
+        .any(|reference| reference.path == space_reference.path);
+
+    if !reference_exists {
+        zaku_store.space_references.push(space_reference);
+
+        ZakuStore::persist(&zaku_store);
+    }
+}
+
+pub fn delete_space_reference(space_reference: SpaceReference) {
+    let mut zaku_store = ZakuStore::acquire_write_lock();
+
+    zaku_store
+        .space_references
+        .retain(|reference| reference.path != space_reference.path);
+
+    if let Some(active_space) = &zaku_store.active_space_reference {
+        if active_space.path == space_reference.path {
+            zaku_store.active_space_reference = None;
         }
     }
 
-    let updated_space_references = {
-        let mut space_references = get_space_references(app_handle.clone(), stores.clone());
-        space_references.retain(|existing_space_reference| {
-            existing_space_reference.path != space_reference.path
-        });
-
-        space_references
-    };
-
-    set_space_references(updated_space_references, app_handle.clone(), stores.clone());
-
-    return ();
-}
-
-pub fn get_space_references(
-    app_handle: AppHandle,
-    stores: State<'_, StoreCollection<Wry>>,
-) -> Vec<SpaceReference> {
-    let app_data_dir = app_handle.path().app_data_dir().unwrap();
-
-    return tauri_plugin_store::with_store(app_handle, stores, app_data_dir, |store| {
-        let empty_array = serde_json::json!([]);
-
-        return Ok(store
-            .get(ZakuStoreKey::SpaceReferences.to_string())
-            .unwrap_or_else(|| &empty_array)
-            .as_array()
-            .unwrap()
-            .iter()
-            .filter_map(|value| {
-                value.as_object().and_then(|object| {
-                    return Some(SpaceReference {
-                        path: object.get("path").unwrap().as_str().unwrap().to_string(),
-                        name: object.get("name").unwrap().as_str().unwrap().to_string(),
-                    });
-                })
-            })
-            .collect());
-    })
-    .unwrap_or_else(|_| Vec::new());
-}
-
-pub fn set_space_references(
-    space_references: Vec<SpaceReference>,
-    app_handle: AppHandle,
-    stores: State<'_, StoreCollection<Wry>>,
-) {
-    let app_data_dir = app_handle.path().app_data_dir().unwrap();
-
-    return tauri_plugin_store::with_store(app_handle, stores, app_data_dir, |store| {
-        store
-            .insert(
-                ZakuStoreKey::SpaceReferences.to_string(),
-                serde_json::json!(space_references),
-            )
-            .unwrap();
-
-        store.save().unwrap();
-
-        return Ok(());
-    })
-    .unwrap();
-}
-
-pub fn insert_into_space_references_if_needed(
-    space_reference: SpaceReference,
-    app_handle: AppHandle,
-    stores: State<'_, StoreCollection<Wry>>,
-) {
-    let mut space_references = get_space_references(app_handle.clone(), stores.clone());
-    let exists_in_space_references = space_references.iter().any(|existing_space_reference| {
-        existing_space_reference.path == space_reference.path.clone()
-    });
-
-    if !exists_in_space_references {
-        space_references.push(SpaceReference {
-            path: space_reference.path.clone(),
-            name: space_reference.name.clone(),
-        });
-
-        set_space_references(space_references, app_handle, stores);
-    }
-
-    return ();
+    ZakuStore::persist(&zaku_store);
 }
