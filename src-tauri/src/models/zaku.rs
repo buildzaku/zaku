55a8b1d4dc8351265e90bc64506dfa99c6526a84
mayank <errmayank@gmail.com>
2024-10-14 21:12:15 +0530

    feat: add custom store for persistence

--- src-tauri/src/models/zaku.rs
+++ src-tauri/src/models/zaku.rs
@@ -16,3 +16,9 @@ pub struct ZakuError {
     pub error: String,
     pub message: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct ZakuStore {
+    pub active_space_reference: Option<SpaceReference>,
+    pub space_references: Vec<SpaceReference>,
+}
