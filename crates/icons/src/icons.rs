4c82f4bccad3014a7f95403dabbdfb1b048cd9b7
Mayank Verma <errmayank@gmail.com>
2026-02-03 16:05:47 +0530

    icons: Setup IconName with path resolution

---
+++ crates/icons/src/icons.rs
@@ -0,0 +1,20 @@
+use std::sync::Arc;
+
+use serde::{Deserialize, Serialize};
+use strum::{EnumIter, EnumString, IntoStaticStr};
+
+#[derive(
+    Debug, PartialEq, Eq, Copy, Clone, EnumIter, EnumString, IntoStaticStr, Serialize, Deserialize,
+)]
+#[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum IconName {
+    Dock,
+}
+
+impl IconName {
+    pub fn path(&self) -> Arc<str> {
+        let file_stem: &'static str = self.into();
+        format!("icons/{file_stem}.svg").into()
+    }
+}
