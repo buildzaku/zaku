3e8cdc636efc537c1a6ea37e89704aa94dc61e02
Mayank Verma <errmayank@gmail.com>
2026-02-03 15:59:01 +0530

    assets: Setup embedded SVG icons

--- crates/comet/src/main.rs
+++ crates/comet/src/main.rs
@@ -8,31 +8,33 @@ use workspace::Workspace;
 actions!(comet, [Quit]);
 
 fn main() {
-    Application::new().run(|cx: &mut App| {
-        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
-        cx.on_action(|_: &Quit, cx: &mut App| {
-            cx.quit();
-        });
-        cx.on_window_closed(|cx| {
-            if cx.windows().is_empty() {
+    Application::new()
+        .with_assets(assets::Assets)
+        .run(|cx: &mut App| {
+            cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+            cx.on_action(|_: &Quit, cx: &mut App| {
                 cx.quit();
-            }
-        })
-        .detach();
+            });
+            cx.on_window_closed(|cx| {
+                if cx.windows().is_empty() {
+                    cx.quit();
+                }
+            })
+            .detach();
 
-        cx.activate(true);
+            cx.activate(true);
 
-        let window_size = size(px(1180.0), px(760.0));
-        let mut bounds = Bounds::centered(None, window_size, cx);
-        bounds.origin.y -= px(36.0);
+            let window_size = size(px(1180.0), px(760.0));
+            let mut bounds = Bounds::centered(None, window_size, cx);
+            bounds.origin.y -= px(36.0);
 
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |_, cx| cx.new(Workspace::new),
-        )
-        .unwrap();
-    });
+            cx.open_window(
+                WindowOptions {
+                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    ..Default::default()
+                },
+                |_, cx| cx.new(Workspace::new),
+            )
+            .unwrap();
+        });
 }
