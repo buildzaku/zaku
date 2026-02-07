f5beda93524b20672cb3e3cbb321db90f08fb92d
Mayank Verma <errmayank@gmail.com>
2026-02-07 15:51:58 +0530

    Move

--- crates/ui/src/ui.rs
+++ crates/ui/src/ui.rs
@@ -1,14 +1,10 @@
-mod button;
-mod button_like;
+mod components;
 mod icon;
-mod icon_button;
 mod stack;
 mod styles;
 
-pub use button::*;
-pub use button_like::*;
+pub use components::*;
 pub use icon::*;
-pub use icon_button::*;
 pub use icons::IconName;
 pub use stack::*;
 pub use styles::*;
