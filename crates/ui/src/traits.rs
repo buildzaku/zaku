b7754793907c338fa284ed24e502d4292cf015ec
Mayank Verma <errmayank@gmail.com>
2026-02-14 11:43:55 +0530

    Cleanup module exports

--- crates/ui/src/traits.rs
+++ crates/ui/src/traits.rs
@@ -4,3 +4,10 @@ pub mod fixed;
 pub mod styled_ext;
 pub mod toggleable;
 pub mod visible_on_hover;
+
+pub use clickable::*;
+pub use disableable::*;
+pub use fixed::*;
+pub use styled_ext::*;
+pub use toggleable::*;
+pub use visible_on_hover::*;
--- crates/ui/src/ui.rs
+++ crates/ui/src/ui.rs
@@ -4,13 +4,9 @@ pub mod prelude;
 mod styles;
 pub mod traits;
 
+pub use icons::IconName;
+
 pub use components::*;
 pub use icon::*;
-pub use icons::IconName;
 pub use styles::*;
-pub use traits::clickable::*;
-pub use traits::disableable::*;
-pub use traits::fixed::*;
-pub use traits::styled_ext::StyledExt;
-pub use traits::toggleable::*;
-pub use traits::visible_on_hover::*;
+pub use traits::*;
