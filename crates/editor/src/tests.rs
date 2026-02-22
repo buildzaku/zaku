40fa7e364fe668e91d439d80da1db3fe42deff7e
Mayank Verma <errmayank@gmail.com>
2026-02-22 20:01:57 +0530

    Use cx

--- crates/editor/src/tests.rs
+++ crates/editor/src/tests.rs
@@ -24,17 +24,17 @@ fn init_test(cx: &mut TestAppContext) {
 #[gpui::test]
 fn test_handle_input_replaces_selection(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("Hello, «worldˇ»!");
-    editor_test_context.dispatch_action(HandleInput("from Zaku".to_string()));
-    editor_test_context.assert_state("Hello, from Zakuˇ!");
+    cx.set_state("Hello, «worldˇ»!");
+    cx.dispatch_action(HandleInput("from Zaku".to_string()));
+    cx.assert_state("Hello, from Zakuˇ!");
 
-    editor_test_context.set_state(indoc! {"
+    cx.set_state(indoc! {"
         Lorem «ipsumˇ» dolor sit amet
     "});
-    editor_test_context.dispatch_action(HandleInput("ips\num".to_string()));
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(HandleInput("ips\num".to_string()));
+    cx.assert_state(indoc! {"
         Lorem ips
         umˇ dolor sit amet
     "});
@@ -43,173 +43,172 @@ fn test_handle_input_replaces_selection(cx: &mut TestAppContext) {
 #[gpui::test]
 fn test_backspace_and_delete_actions(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("Hello, woˇrld!");
-    editor_test_context.dispatch_action(Backspace);
-    editor_test_context.assert_state("Hello, wˇrld!");
+    cx.set_state("Hello, woˇrld!");
+    cx.dispatch_action(Backspace);
+    cx.assert_state("Hello, wˇrld!");
 
-    editor_test_context.dispatch_action(Delete);
-    editor_test_context.assert_state("Hello, wˇld!");
+    cx.dispatch_action(Delete);
+    cx.assert_state("Hello, wˇld!");
 }
 
 #[gpui::test]
 fn test_move_to_beginning_of_line_toggles_indent(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("•••Lorem ipsum dolor sit ametˇ");
+    cx.set_state("•••Lorem ipsum dolor sit ametˇ");
     let move_to_beginning = MoveToBeginningOfLine {
         stop_at_soft_wraps: true,
         stop_at_indent: true,
     };
 
-    editor_test_context.dispatch_action(move_to_beginning.clone());
-    editor_test_context.assert_state("•••ˇLorem ipsum dolor sit amet");
+    cx.dispatch_action(move_to_beginning.clone());
+    cx.assert_state("•••ˇLorem ipsum dolor sit amet");
 
-    editor_test_context.dispatch_action(move_to_beginning);
-    editor_test_context.assert_state("ˇ•••Lorem ipsum dolor sit amet");
+    cx.dispatch_action(move_to_beginning);
+    cx.assert_state("ˇ•••Lorem ipsum dolor sit amet");
 }
 
 #[gpui::test]
 fn test_delete_to_beginning_of_line_respects_indent(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("•••Lorem ipsum dolor sit ametˇ");
-    editor_test_context.dispatch_action(DeleteToBeginningOfLine {
+    cx.set_state("•••Lorem ipsum dolor sit ametˇ");
+    cx.dispatch_action(DeleteToBeginningOfLine {
         stop_at_indent: true,
     });
-    editor_test_context.assert_state("•••ˇ");
+    cx.assert_state("•••ˇ");
 }
 
 #[gpui::test]
 fn test_undo_redo_restores_cursor(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("Hello, woˇrld!");
-    editor_test_context.dispatch_action(HandleInput("d".to_string()));
-    editor_test_context.assert_state("Hello, wodˇrld!");
+    cx.set_state("Hello, woˇrld!");
+    cx.dispatch_action(HandleInput("d".to_string()));
+    cx.assert_state("Hello, wodˇrld!");
 
-    editor_test_context.dispatch_action(Undo);
-    editor_test_context.assert_state("Hello, woˇrld!");
+    cx.dispatch_action(Undo);
+    cx.assert_state("Hello, woˇrld!");
 
-    editor_test_context.dispatch_action(Redo);
-    editor_test_context.assert_state("Hello, wodˇrld!");
+    cx.dispatch_action(Redo);
+    cx.assert_state("Hello, wodˇrld!");
 }
 
 #[gpui::test]
 fn test_undo_redo_restores_selection(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("Hello, «worldˇ»!");
-    editor_test_context.dispatch_action(HandleInput("from Zaku".to_string()));
-    editor_test_context.assert_state("Hello, from Zakuˇ!");
+    cx.set_state("Hello, «worldˇ»!");
+    cx.dispatch_action(HandleInput("from Zaku".to_string()));
+    cx.assert_state("Hello, from Zakuˇ!");
 
-    editor_test_context.dispatch_action(Undo);
-    editor_test_context.assert_state("Hello, «worldˇ»!");
+    cx.dispatch_action(Undo);
+    cx.assert_state("Hello, «worldˇ»!");
 
-    editor_test_context.dispatch_action(Redo);
-    editor_test_context.assert_state("Hello, from Zakuˇ!");
+    cx.dispatch_action(Redo);
+    cx.assert_state("Hello, from Zakuˇ!");
 }
 
 #[gpui::test]
 fn test_undo_redo_selection(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("Hello, woˇrld!");
-    editor_test_context.dispatch_action(MoveRight);
-    editor_test_context.assert_state("Hello, worˇld!");
+    cx.set_state("Hello, woˇrld!");
+    cx.dispatch_action(MoveRight);
+    cx.assert_state("Hello, worˇld!");
 
-    editor_test_context.dispatch_action(MoveLeft);
-    editor_test_context.assert_state("Hello, woˇrld!");
+    cx.dispatch_action(MoveLeft);
+    cx.assert_state("Hello, woˇrld!");
 
-    editor_test_context.dispatch_action(UndoSelection);
-    editor_test_context.assert_state("Hello, worˇld!");
+    cx.dispatch_action(UndoSelection);
+    cx.assert_state("Hello, worˇld!");
 
-    editor_test_context.dispatch_action(RedoSelection);
-    editor_test_context.assert_state("Hello, woˇrld!");
+    cx.dispatch_action(RedoSelection);
+    cx.assert_state("Hello, woˇrld!");
 }
 
 #[gpui::test]
 fn test_copy_cut_paste_actions(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state("Hello, «worldˇ»!");
-    editor_test_context.dispatch_action(Copy);
-    let clipboard_text = editor_test_context
+    cx.set_state("Hello, «worldˇ»!");
+    cx.dispatch_action(Copy);
+    let clipboard_text = cx
         .cx
         .read_from_clipboard()
         .and_then(|item: ClipboardItem| item.text());
     assert_eq!(clipboard_text.as_deref(), Some("world"));
 
-    editor_test_context.dispatch_action(Cut);
-    editor_test_context.assert_state("Hello, ˇ!");
-    let clipboard_text = editor_test_context
+    cx.dispatch_action(Cut);
+    cx.assert_state("Hello, ˇ!");
+    let clipboard_text = cx
         .cx
         .read_from_clipboard()
         .and_then(|item: ClipboardItem| item.text());
     assert_eq!(clipboard_text.as_deref(), Some("world"));
 
-    editor_test_context
-        .cx
+    cx.cx
         .write_to_clipboard(ClipboardItem::new_string("hello world".to_string()));
-    editor_test_context.dispatch_action(Paste);
-    editor_test_context.assert_state("Hello, hello worldˇ!");
+    cx.dispatch_action(Paste);
+    cx.assert_state("Hello, hello worldˇ!");
 }
 
 #[gpui::test]
 fn test_move_cursor(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state(indoc! {"
+    cx.set_state(indoc! {"
         ˇaaaaaa
         \t\taaaaaa
         aaaaaa\
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         aaaaaa
         ˇ\t\taaaaaa
         aaaaaa\
     "});
 
-    editor_test_context.dispatch_action(MoveRight);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveRight);
+    cx.assert_state(indoc! {"
         aaaaaa
         \tˇ\taaaaaa
         aaaaaa\
     "});
 
-    editor_test_context.dispatch_action(MoveLeft);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveLeft);
+    cx.assert_state(indoc! {"
         aaaaaa
         ˇ\t\taaaaaa
         aaaaaa\
     "});
 
-    editor_test_context.dispatch_action(MoveUp);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveUp);
+    cx.assert_state(indoc! {"
         ˇaaaaaa
         \t\taaaaaa
         aaaaaa\
     "});
 
-    editor_test_context.dispatch_action(MoveToEnd);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveToEnd);
+    cx.assert_state(indoc! {"
         aaaaaa
         \t\taaaaaa
         aaaaaaˇ\
     "});
 
-    editor_test_context.dispatch_action(MoveToBeginning);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveToBeginning);
+    cx.assert_state(indoc! {"
         ˇaaaaaa
         \t\taaaaaa
         aaaaaa\
@@ -219,66 +218,66 @@ fn test_move_cursor(cx: &mut TestAppContext) {
 #[gpui::test]
 fn test_move_cursor_multibyte(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state(indoc! {"
+    cx.set_state(indoc! {"
         ˇ🌑🌒🌓🌔🌕🌖
         abcde
         абвгд
     "});
 
-    editor_test_context.dispatch_action(MoveRight);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveRight);
+    cx.assert_state(indoc! {"
         🌑ˇ🌒🌓🌔🌕🌖
         abcde
         абвгд
     "});
 
-    editor_test_context.dispatch_action(MoveRight);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveRight);
+    cx.assert_state(indoc! {"
         🌑🌒ˇ🌓🌔🌕🌖
         abcde
         абвгд
     "});
 
-    editor_test_context.dispatch_action(MoveRight);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveRight);
+    cx.assert_state(indoc! {"
         🌑🌒🌓ˇ🌔🌕🌖
         abcde
         абвгд
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         🌑🌒🌓🌔🌕🌖
         abcdeˇ
         абвгд
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         🌑🌒🌓🌔🌕🌖
         abcde
         абвгдˇ
     "});
 
-    editor_test_context.dispatch_action(MoveLeft);
-    editor_test_context.dispatch_action(MoveLeft);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveLeft);
+    cx.dispatch_action(MoveLeft);
+    cx.assert_state(indoc! {"
         🌑🌒🌓🌔🌕🌖
         abcde
         абвˇгд
     "});
 
-    editor_test_context.dispatch_action(MoveUp);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveUp);
+    cx.assert_state(indoc! {"
         🌑🌒🌓🌔🌕🌖
         abcˇde
         абвгд
     "});
 
-    editor_test_context.dispatch_action(MoveUp);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveUp);
+    cx.assert_state(indoc! {"
         🌑ˇ🌒🌓🌔🌕🌖
         abcde
         абвгд
@@ -288,9 +287,9 @@ fn test_move_cursor_multibyte(cx: &mut TestAppContext) {
 #[gpui::test]
 fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
     init_test(cx);
-    let mut editor_test_context = EditorTestContext::new(cx);
+    let mut cx = EditorTestContext::new(cx);
 
-    editor_test_context.set_state(indoc! {"
+    cx.set_state(indoc! {"
         ⓐⓑⓒⓓⓔˇ
         abcd
         αβγ
@@ -298,8 +297,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔ\
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcdˇ
         αβγ
@@ -307,8 +306,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔ\
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcd
         αβγˇ
@@ -316,8 +315,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔ\
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcd
         αβγ
@@ -325,8 +324,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔ\
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcd
         αβγ
@@ -334,8 +333,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔˇ\
     "});
 
-    editor_test_context.dispatch_action(MoveDown);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveDown);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcd
         αβγ
@@ -343,8 +342,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔˇ\
     "});
 
-    editor_test_context.dispatch_action(MoveUp);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveUp);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcd
         αβγ
@@ -352,8 +351,8 @@ fn test_move_cursor_different_line_lengths(cx: &mut TestAppContext) {
         ⓐⓑⓒⓓⓔ\
     "});
 
-    editor_test_context.dispatch_action(MoveUp);
-    editor_test_context.assert_state(indoc! {"
+    cx.dispatch_action(MoveUp);
+    cx.assert_state(indoc! {"
         ⓐⓑⓒⓓⓔ
         abcd
         αβγˇ
