ac4ad98945a027fb00e891b16115c2840acf7e14
Mayank Verma <errmayank@gmail.com>
2026-06-01 19:10:44 +0530

    Use 9.5 inset

--- crates/ui/src/utils/constants.rs
+++ crates/ui/src/utils/constants.rs
@@ -3,7 +3,7 @@ use gpui::Pixels;
 #[cfg(target_os = "macos")]
 pub const MACOS_TRAFFIC_LIGHT_PADDING: f32 = 78.0;
 #[cfg(target_os = "macos")]
-pub const MACOS_TRAFFIC_LIGHT_INSET: (Pixels, Pixels) = (gpui::px(10.0), gpui::px(10.0));
+pub const MACOS_TRAFFIC_LIGHT_INSET: (Pixels, Pixels) = (gpui::px(9.5), gpui::px(9.5));
 
 pub fn title_bar_height(rem_size: Pixels) -> Pixels {
     (1.75 * rem_size).max(gpui::px(32.0))
