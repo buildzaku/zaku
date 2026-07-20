62a80fd191e073bcff9eae4fadea7f259cbc8135
Mayank Verma <errmayank@gmail.com>
2026-07-20 18:57:52 +0530

    Show file names instead of count

--- script/check.ps1
+++ script/check.ps1
@@ -7,6 +7,9 @@ if ($args.Length -gt 0) {
     exit 1
 }
 
-$powerShellScripts = @(Get-ChildItem -Path $PSScriptRoot -Filter "*.ps1" -File)
-Write-Information "Checking $($powerShellScripts.Count) PowerShell scripts" -InformationAction Continue
+$powerShellScripts = @(Get-ChildItem -Path $PSScriptRoot -Filter "*.ps1" -File | Sort-Object -Property Name)
+Write-Information "Checking PowerShell scripts:" -InformationAction Continue
+foreach ($powerShellScript in $powerShellScripts) {
+    Write-Information "  .\script\$($powerShellScript.Name)" -InformationAction Continue
+}
 Invoke-ScriptAnalyzer -Path "$PSScriptRoot/*.ps1" -Severity Error, Warning -EnableExit
