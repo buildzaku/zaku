98f725077639c4fadc561f084961e8f2220b817c
Mayank Verma <errmayank@gmail.com>
2026-08-18 00:27:02 +0530

    telemetry crates with events

---
+++ crates/telemetry/src/telemetry.rs
@@ -0,0 +1,62 @@
+pub use serde_json;
+pub use telemetry_events::FlexibleEvent as Event;
+
+use futures::channel::mpsc;
+use std::sync::OnceLock;
+
+/// Create a telemetry event and send it to the telemetry queue.
+///
+/// ```
+/// telemetry::event!("App Opened");
+/// telemetry::event!("Minidump Uploaded", crashed_version = "26.0");
+/// ```
+#[macro_export]
+macro_rules! event {
+    ($name:expr) => {{
+        let event = $crate::Event {
+            event_type: $name.to_string(),
+            event_properties: std::collections::HashMap::new(),
+        };
+        $crate::send_event(event);
+    }};
+    ($name:expr, $($key:ident $(= $value:expr)?),+ $(,)?) => {{
+        let event = $crate::Event {
+            event_type: $name.to_string(),
+            event_properties: std::collections::HashMap::from([
+                $(
+                    (stringify!($key).to_string(),
+                        $crate::serde_json::value::to_value(&$crate::serialize_property!($key $(= $value)?))
+                            .unwrap_or($crate::serde_json::Value::Null)
+                    ),
+                )+
+            ]),
+        };
+        $crate::send_event(event);
+    }};
+}
+
+#[macro_export]
+macro_rules! serialize_property {
+    ($key:ident) => {
+        $key
+    };
+    ($key:ident = $value:expr) => {
+        $value
+    };
+}
+
+pub fn send_event(event: Event) {
+    if let Some(queue) = TELEMETRY_QUEUE.get()
+        && queue.unbounded_send(event).is_err()
+    {
+        log::trace!("Failed to send telemetry event because the queue is closed");
+    }
+}
+
+pub fn init(sender: mpsc::UnboundedSender<Event>) {
+    if TELEMETRY_QUEUE.set(sender).is_err() {
+        log::warn!("Telemetry queue is already initialized");
+    }
+}
+
+static TELEMETRY_QUEUE: OnceLock<mpsc::UnboundedSender<Event>> = OnceLock::new();
---
+++ crates/telemetry_events/src/telemetry_events.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EventRequestBody {
+    /// Identifier unique to each system Zaku is installed on.
+    pub system_id: Option<String>,
+    /// Identifier unique to each Zaku installation for a release channel.
+    pub installation_id: Option<String>,
+    /// Identifier unique to each application session.
+    pub session_id: Option<String>,
+    /// Application version.
+    pub app_version: String,
+    /// Operating system.
+    pub os_name: String,
+    /// Operating system version.
+    pub os_version: Option<String>,
+    /// Application architecture.
+    pub architecture: String,
+    /// Application release channel.
+    pub release_channel: Option<String>,
+    /// Events included in the batch.
+    pub events: Vec<EventWrapper>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EventWrapper {
+    /// Duration since the first event in the batch.
+    pub elapsed_ms: u32,
+    /// The event itself.
+    #[serde(flatten)]
+    pub event: Event,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum Event {
+    Flexible(FlexibleEvent),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct FlexibleEvent {
+    /// Name of the event.
+    pub event_type: String,
+    /// Properties associated with the event.
+    pub event_properties: HashMap<String, serde_json::Value>,
+}
