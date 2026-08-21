fe4ce5731f26292f2cd921b457519e06b1a6887c
Mayank Verma <errmayank@gmail.com>
2026-08-21 21:53:38 +0530

    http_client

--- crates/client/src/client.rs
+++ crates/client/src/client.rs
@@ -11,30 +11,33 @@ use http_client::{HttpClient, HttpClientWithUrl};
 use crate::telemetry::Telemetry;
 
 pub struct Client {
-    http: Arc<HttpClientWithUrl>,
+    http_client: Arc<HttpClientWithUrl>,
     telemetry: Arc<Telemetry>,
 }
 
 impl Client {
-    pub fn new(http: Arc<dyn HttpClient>, cx: &mut App) -> Arc<Self> {
-        let http = Arc::new(HttpClientWithUrl::new(http, metadata::ZAKU_SERVER_URL));
+    pub fn new(http_client: Arc<dyn HttpClient>, cx: &mut App) -> Arc<Self> {
+        let http_client = Arc::new(HttpClientWithUrl::new(
+            http_client,
+            metadata::ZAKU_SERVER_URL,
+        ));
 
         Arc::new(Self {
-            telemetry: Telemetry::new(Arc::new(RealSystemClock), http.clone(), cx),
-            http,
+            telemetry: Telemetry::new(Arc::new(RealSystemClock), http_client.clone(), cx),
+            http_client,
         })
     }
 
     #[cfg(any(test, feature = "test"))]
-    pub fn test_new(http: Arc<HttpClientWithUrl>, cx: &mut App) -> Arc<Self> {
+    pub fn test_new(http_client: Arc<HttpClientWithUrl>, cx: &mut App) -> Arc<Self> {
         Arc::new(Self {
-            telemetry: Telemetry::new(Arc::new(FakeSystemClock::new()), http.clone(), cx),
-            http,
+            telemetry: Telemetry::new(Arc::new(FakeSystemClock::new()), http_client.clone(), cx),
+            http_client,
         })
     }
 
     pub fn http_client(&self) -> Arc<HttpClientWithUrl> {
-        self.http.clone()
+        self.http_client.clone()
     }
 
     pub fn telemetry(&self) -> &Arc<Telemetry> {
